2a71b4c7177c21ab1dddc42a4393fb21235ddedf
Matteo Komadina <matteo.geffroy@gmail.com>
2021-03-24 14:53:44 +0100

    zero itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,14 +6,16 @@
 /*   By: mkomadin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 11:21:59 by mkomadin          #+#    #+#             */
-/*   Updated: 2021/03/24 14:27:42 by mkomadin         ###   ########lyon.fr   */
+/*   Updated: 2021/03/24 14:53:02 by mkomadin         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-static void	init(int n, long tmp, int *count)
+static char	*init(int n, long tmp, int *count)
 {
+	char	*res;
+
 	tmp = n;
 	if (n >= 0)
 		*count = 0;
@@ -26,6 +28,10 @@ static void	init(int n, long tmp, int *count)
 		tmp /= 10;
 		(*count)++;
 	}
+	res = malloc(sizeof(*res) * (*count) + 1);
+	if (!res)
+		return (0);
+	return (res);
 }
 
 char	*ft_itoa(int n)
@@ -35,10 +41,7 @@ char	*ft_itoa(int n)
 	char	*res;
 
 	tmp = 0;
-	init(n, tmp, &count);
-	res = malloc(sizeof(*res) * count + 1);
-	if (!res)
-		return (0);
+	res = init(n, tmp, &count);
 	res[count] = '\0';
 	tmp = n;
 	if (tmp < 0)
@@ -47,7 +50,10 @@ char	*ft_itoa(int n)
 		res[0] = '-';
 	}
 	if (tmp == 0)
+	{
 		res[0] = '0';
+		res[1] = '\0';
+	}
 	while (tmp > 0)
 	{
 		res[--count] = (char)(48 + tmp % 10);
